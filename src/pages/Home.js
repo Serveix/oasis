aee52a87d97810154672f2216b3644f24e16a19f
Eli <carloselilopezt@gmail.com>
2021-03-25 22:12:14 -0600

    Code with a useEffect bug

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,16 @@
-
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Navbar, Footer, Loading } from "../components";
 import tools from "../data/tools.json";
 import _ from "lodash";
-import firebase from "../data/firebase";
-import 'firebase/functions';
 import logo from "../static/oasis-logo.png";
-import "../style/App.css";
 import { filterToolsByCategory } from "../utils/filterTools";
 import { colours } from "../lib/constants.js";
 import InfiniteScroll from "react-infinite-scroll-component";
+import firebase from "firebase";
+import 'firebase/functions';
+import "../style/App.css";
+import { doc } from "prettier";
 
 const Home = () => {
   // makes a list of just the categories of the tools
@@ -18,118 +18,144 @@ const Home = () => {
   const [list, setList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [hasMoreRepos, setHasMoreRepos] = useState(true);
-
-  const allCategories = list
-    .filter(project => project.language != null)
-    .map(project => project.language);
+  const allCategories = list.filter(project => project.language != null).map(project => project.language);
   const countCategories = _.countBy(allCategories);
   const [currCategory, setCurrCategory] = useState("All");
   const [searchQuery, setSearchQuery] = useState("");
   const [visibleTools, setVisibleTools] = useState(tools);
   const user = firebase.auth().currentUser;
+  const changeSearch = event => setSearchQuery(event.target.value);
 
-  // if searchQuery or currCategory changes, then update visibleTools
-  useEffect(() => {
-    setVisibleTools(filterToolsByCategory(tools, currCategory));
-  }, [searchQuery, currCategory]);
-
-  const changeSearch = event => {
-    setSearchQuery(event.target.value);
-  };
+  useEffect(() => setVisibleTools(filterToolsByCategory(tools, currCategory)), [searchQuery, currCategory]);
 
   useEffect(() => {
-    const getRepos = async () => {
-      try {
-        db.collection("repos")
-          .orderBy("date_added")
-          .limit(8)
-          .onSnapshot(snapshot => {
-            let projects = [];
+    const fetchData = async () => {
+      const reposRef = db.collection("repos")
+        .orderBy("date_added")
+        .limit(2);
+      const repos = await reposRef.get();
+      const repositoryObjects = repos.docs.map(repoDoc => {
+  
+        return {
+          id: repoDoc.id,
+          userLikes: null, // this is updated later by the user, null is "loading" status
+          ...repoDoc.data()
+        }
+      });
+  
+      setList(repositoryObjects);
+      setIsLoading(false);
+  
+      console.log(repos.docs)
+  
+      repositoryObjects.forEach(docSnap => {
+        const docId = docSnap.id;
+        console.log("add snapshot")
+  
+        db.collection("project_likes").doc(docId).onSnapshot(likeDocSnap => {
+          console.log("FIRST load doc", docId, "length:", list.length)
+          if (list.length < 1) {
+            return
+          }
+  
+          setList(list.map(repo => {
+            if (!likeDocSnap.exists) {
+              repo["userLikes"] = 0
+            }
+            else if (repo.id === likeDocSnap.id) {
+              repo["userLikes"] = likeDocSnap.likesCount
+            }
+  
+            return repo;
+          }));
+          setIsLoading(false);
+        });
+      });
+  
+    }
 
-            snapshot.forEach(doc => {
-              projects.push({
-                id: doc.id,
-                ...doc.data()
-              });
-            });
-
-            // for (const doc of snapshot.docs)
-            //   projects = _.concat(projects, {
-            //     id: doc.id,
-            //     ...doc.data()
-            //   });
-
-            setList(projects);
-            setIsLoading(false);
-          });
-      } catch (err) {
-        console.error(err);
-      }
-    };
-    getRepos();
-  }, []);
+    fetchData();
+  }, [db, list]);
 
   const fetchMoreData = async () => {
     const lastDoc = list[list.length - 1];
+    const reposRef = db.collection("repos")
+      .orderBy("date_added")
+      .limit(8)
+      .startAfter(lastDoc.date_added);
 
-    try {
-      db.collection("repos")
-        .orderBy("date_added")
-        .limit(8)
-        .startAfter(lastDoc.date_added)
-        .onSnapshot(snapshot => {
-          let projects = [...list];
+    const repos = await reposRef.get()
 
-          if (snapshot.docs.length) {
-            snapshot.forEach(doc => {
-              projects.push({
-                id: doc.id,
-                ...doc.data()
-              });
-            });
+    if (repos.docs.length < 1) {
+      return setHasMoreRepos(false);
+    }
 
-            setList(projects);
-          } else {
-            setHasMoreRepos(false);
+    list.push(...repos.docs.map(repoDoc => {
+      return {
+        id: repoDoc.id,
+        ...repoDoc.data()
+      }
+    }));
+
+    repos.forEach(docSnap => {
+      console.log("adding listener")
+      const docId = docSnap.id;
+      db.collection("project_likes").doc(docId).onSnapshot(likeDocSnap => {
+        console.log("Project likes doc ", docId, list.length, "snap:", likeDocSnap.id)
+        if (list.length < 1) {
+          return
+        }
+
+        setList(list.map(repo => {
+          if (!likeDocSnap.exists) {
+            repo["userLikes"] = 0
+          }
+          else if (repo.id === likeDocSnap.id) {
+            repo["userLikes"] = likeDocSnap.likesCount
           }
 
-          // for (const doc of snapshot.docs)
-          //   projects = _.concat(projects, {
-          //     id: doc.id,
-          //     ...doc.data()
-          //   });
-        });
-    } catch (err) {
-      console.err(err);
-    }
+          return repo;
+        }));
+
+      });
+    });
+
+    setList(list);
+    setIsLoading(false);
+
+    // db.collection("repos")
+    //   .orderBy("date_added")
+    //   .limit(8)
+    //   .startAfter(lastDoc.date_added)
+    //   .onSnapshot(snapshot => {
+    //     let projects = [...list];
+
+    //     if (snapshot.docs.length) {
+    //       snapshot.forEach(doc => projects.push({
+    //         id: doc.id,
+    //         ...doc.data()
+    //       }));
+
+    //       setList(projects);
+    //     } else {
+    //       setHasMoreRepos(false);
+    //     }
+    //   },
+    //     err => console.error(err));
   };
 
-  function formatNumber(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
+  const formatNumber = x => x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
-function toggleLike(id){
-  // const increment = firebase.firestore.FieldValue.increment(1)
-  // const repoRef = db.collection("repos").doc(id)
-
-  // repoRef.update({likes: increment})
-  console.log("LOADING...");
-  const toggleLike = firebase.functions().httpsCallable("toggleProjectLiked");
-
-  toggleLike({
-    projectId: id,
-  })
-    .then(response => console.log(response.data))
-    // .finally(() => console.log("END LOADING"))
-    .catch(console.error);
-}
+  const toggleLike = async id => {
+    const toggleProjectLiked = firebase.functions().httpsCallable("toggleProjectLiked");
+    await toggleProjectLiked({ projectId: id });
+  }
 
   const Button = ({ category }) => {
     return (
       <button
-        className={`filter-button ${
-          category === currCategory ? "filter-active" : ""
-        }`}
+        className={`filter-button ${category === currCategory ? "filter-active" : ""
+          }`}
         title={category}
         onClick={() => {
           setCurrCategory(category);
@@ -142,9 +168,7 @@ function toggleLike(id){
 
   return (
     <div>
-      
       <Navbar />
-
       <header>
         <div className="header-content">
           <Link to="/">
@@ -175,7 +199,7 @@ function toggleLike(id){
               onChange={changeSearch}
             />
             <div className="filter-wrapper">
-              <Button category="All"/>
+              <Button category="All" />
               {Object.keys(countCategories).map(category => (
                 <Button
                   key={category}
@@ -218,78 +242,62 @@ function toggleLike(id){
                 //   to={`/${project.owner}/${project.name}`}
                 //   rel="noreferrer"
                 // >
-                  <div className="repo">
-                    {project.fork === true && (
-                      <div
-                        className="fork-icon"
-                        title={`${project.full_name} is a forked repository`}
+                <div className="repo">
+                  {project.fork === true && (
+                    <div
+                      className="fork-icon"
+                      title={`${project.full_name} is a forked repository`}
+                    >
+                      <svg
+                        viewBox="0 0 16 16"
+                        version="1.1"
+                        width="20"
+                        height="20"
+                        aria-hidden="true"
+                        fill="white"
                       >
-                        <svg
-                          viewBox="0 0 16 16"
-                          version="1.1"
-                          width="20"
-                          height="20"
-                          aria-hidden="true"
-                          fill="white"
-                        >
-                          <path
-                            fill-rule="evenodd"
-                            d="M5 3.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm0 2.122a2.25 2.25 0 10-1.5 0v.878A2.25 2.25 0 005.75 8.5h1.5v2.128a2.251 2.251 0 101.5 0V8.5h1.5a2.25 2.25 0 002.25-2.25v-.878a2.25 2.25 0 10-1.5 0v.878a.75.75 0 01-.75.75h-4.5A.75.75 0 015 6.25v-.878zm3.75 7.378a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm3-8.75a.75.75 0 100-1.5.75.75 0 000 1.5z"
-                          ></path>
+                        <path
+                          fill-rule="evenodd"
+                          d="M5 3.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm0 2.122a2.25 2.25 0 10-1.5 0v.878A2.25 2.25 0 005.75 8.5h1.5v2.128a2.251 2.251 0 101.5 0V8.5h1.5a2.25 2.25 0 002.25-2.25v-.878a2.25 2.25 0 10-1.5 0v.878a.75.75 0 01-.75.75h-4.5A.75.75 0 015 6.25v-.878zm3.75 7.378a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm3-8.75a.75.75 0 100-1.5.75.75 0 000 1.5z"
+                        ></path>
+                      </svg>
+                    </div>
+                  )}
+
+                  <img
+                    alt={`${project.owner.toLowerCase()}'s logo`}
+                    className="display"
+                    src={project.avatar}
+                    title={project.owner}
+                  />
+                  <p key={index}>
+                    <span className="owner">{project.owner}</span>/
+                      <span className="name">{project.name}</span>
+                  </p>
+
+                  {project.desc != null && <small>{project.desc}</small>}
+
+                  {project.desc === null && <small>No description.</small>}
+
+                  <div className="category-wrapper">
+                    {project.language in colours && (
+                      <button className="language">
+                        <svg viewBox="0 0 80 80" width="10" height="10">
+                          <circle
+                            style={{ fill: colours[project.language] }}
+                            className="circle"
+                            cx="40"
+                            cy="40"
+                            r="38"
+                          />
                         </svg>
-                      </div>
+                          &nbsp;
+                        {project.language}
+                      </button>
                     )}
 
-                    <img
-                      alt={`${project.owner.toLowerCase()}'s logo`}
-                      className="display"
-                      src={project.avatar}
-                      title={project.owner}
-                    />
-                    <p key={index}>
-                      <span className="owner">{project.owner}</span>/
-                      <span className="name">{project.name}</span>
-                    </p>
-
-                    {project.desc != null && <small>{project.desc}</small>}
-
-                    {project.desc === null && <small>No description.</small>}
-
-                    <div className="category-wrapper">
-                      {project.language in colours && (
-                        <button className="language">
-                          <svg viewBox="0 0 80 80" width="10" height="10">
-                            <circle
-                              style={{ fill: colours[project.language] }}
-                              className="circle"
-                              cx="40"
-                              cy="40"
-                              r="38"
-                            />
-                          </svg>
-                          &nbsp;
-                          {project.language}
-                        </button>
-                      )}
-
-                      {!(project.language in colours) &&
-                        project.language != null && (
-                          <button className="language">
-                            <svg viewBox="0 0 80 80" width="10" height="10">
-                              <circle
-                                style={{ fill: "#fff" }}
-                                className="circle"
-                                cx="40"
-                                cy="40"
-                                r="38"
-                              />
-                            </svg>
-                            &nbsp;
-                            {project.language}
-                          </button>
-                        )}
-
-                      {project.language === null && (
+                    {!(project.language in colours) &&
+                      project.language != null && (
                         <button className="language">
                           <svg viewBox="0 0 80 80" width="10" height="10">
                             <circle
@@ -300,33 +308,49 @@ function toggleLike(id){
                               r="38"
                             />
                           </svg>
-                          &nbsp; N/A
+                            &nbsp;
+                          {project.language}
                         </button>
                       )}
 
-                      {
-                      
+                    {project.language === null && (
+                      <button className="language">
+                        <svg viewBox="0 0 80 80" width="10" height="10">
+                          <circle
+                            style={{ fill: "#fff" }}
+                            className="circle"
+                            cx="40"
+                            cy="40"
+                            r="38"
+                          />
+                        </svg>
+                          &nbsp; N/A
+                      </button>
+                    )}
+
+                    {
+
                       (project.issues > 1000) ? <button className="issues">
                         🚨 1k+ issues
-                      </button> : 
-                      
-                      <button className="issues">
-                        🚨 {project.issues} issues
-                      </button>
-                      }
+                      </button> :
 
-                      <br />
-                      <button className="stars">
-                        ⭐ {formatNumber(project.stars)} stars
+                        <button className="issues">
+                          🚨 {project.issues} issues
+                      </button>
+                    }
+
+                    <br />
+                    <button className="stars">
+                      ⭐ {formatNumber(project.stars)} stars
                       </button>
 
-                      
-                      <button className="like" style={{cursor: user ? 'cursor' : 'text'}} onClick={user ? () => toggleLike(project.id) : ''}>
-                      🧡 {project.likes}
+
+                    <button className="like" style={{ cursor: user ? 'cursor' : 'text' }} onClick={user ? () => toggleLike(project.id) : ''}>
+                      🧡 {project.likesCount || "loading"}
                     </button>
-                    </div>
                   </div>
-                
+                </div>
+
               ))}
               {/* </Link> */}
             </div>
